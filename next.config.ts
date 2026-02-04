d26d1dffd2100e03ba0a926b749fc636ed3150bd
Arnav Jain <arnavjain0530@gmail.com>
2026-02-04 21:50:26 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Your Next.js config
+  output: "export",
+  basePath: "/prsnl",
+  assetPrefix: "/prsnl/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
